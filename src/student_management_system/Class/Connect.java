5e400d3edc7ef93445680dc5e22a74911408053e
endsy2 <155872351+endsy2@users.noreply.github.com>
2024-07-30 13:41:58 +0700

    delete

--- src/student_management_system/Class/Connect.java
+++ src/student_management_system/Class/Connect.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
 public class Connect {
     public static final String url="jdbc:mysql://127.0.0.1:3306/studentmanagementsystem";
     public static final String user="root";
-    public static final String password="062005hak";
+    public static final String password="kongming16";
     public static Connection getConnection(){
         Connection connection=null;
         try {
