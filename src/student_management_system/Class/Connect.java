f563eac43e6a636310ca38343f57d9b6d2966af7
ChanSenghak <163015944+ChanSenghak@users.noreply.github.com>
2024-08-06 21:53:27 +0700

    test

--- src/student_management_system/Class/Connect.java
+++ src/student_management_system/Class/Connect.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
 public class Connect {
     public static final String url="jdbc:mysql://127.0.0.1:3306/studentmanagementsystem";
     public static final String user="root";
-    public static final String password="kongming16";
+    public static final String password="062005hak";
     public static Connection getConnection(){
         Connection connection=null;
         try {
@@ -26,7 +26,11 @@ public class Connect {
         }
         catch(ClassNotFoundException | SQLException e){
             System.out.println(e.getMessage());
+<<<<<<< Updated upstream:src/student_management_system/Class/Connect.java
             
+=======
+            JOptionPane.showMessageDialog(null,e.getMessage(),"Message",JOptionPane.ERROR_MESSAGE);
+>>>>>>> Stashed changes:src/student_management_system/Connect.java
         }
         return connection;
     }
